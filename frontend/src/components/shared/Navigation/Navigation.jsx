31f691b3ed0780f38cce858d4134d9e1c07b4c2f
jayesh-unde <113255761+jayesh-unde@users.noreply.github.com>
2024-07-13 19:34:09 +0530

    Update Navigation.jsx

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -41,10 +41,7 @@ const Navigation = () => {
                     <Link to={`/profile/${user?.name}`}>
                         <img
                             className={styles.avatar}
-                            src={
-                                user.avatar
-                                    ? user.avatar
-                                    : '/images/monkey-avatar.png'
+                            src={ '/images/monkey-avatar.png'
                             }
                             width="40"
                             height="40"
